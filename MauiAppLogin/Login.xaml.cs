3d90242e16b013502bec1becc612e29d67e80619
GuillhermeHenrique <piresguilherme003@gmail.com>
2024-10-26 20:29:40 -0300

    Finalização do projeto

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -6,4 +6,56 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+		try
+		{
+
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+			{
+
+				new DadosUsuario()
+				{
+					Usuario = "jose",
+					Senha = "123"
+				},
+
+                new DadosUsuario()
+                {
+                    Usuario = "maria",
+                    Senha = "321"
+                }
+            };
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+            //LINQ
+            bool encontrou = lista_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha));
+
+            if (encontrou)
+            {
+                SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+                App.Current.MainPage = new Protegido();
+
+            }
+            else
+            {
+                throw new Exception("Usuário ou senha incorretos.");
+            }
+
+
+        }
+        catch (Exception ex)
+		{
+			DisplayAlert("ops", ex.Message, "Fechar");
+		}
+
+    }
 }
--- MauiAppLogin/Protegido.xaml.cs
+++ MauiAppLogin/Protegido.xaml.cs
@@ -5,5 +5,28 @@ public partial class Protegido : ContentPage
 	public Protegido()
 	{
 		InitializeComponent();
+
+		string? usuario_logado = null;
+
+		Task.Run(async () =>
+		{
+			usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+			lbl_boasvindas.Text = $"Bem-vindo (a) {usuario_logado}";
+		});
+		
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+		bool confirmacao = await DisplayAlert("Tem certeza?", "Sair do APP?", "SIM", "NÃO");
+
+		if (confirmacao) 
+		{
+			SecureStorage.Default.Remove("usuario_logado");
+			App.Current.MainPage = new Login();
+
+		}
+
+    }
 }
